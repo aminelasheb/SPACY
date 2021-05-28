cad39a9888815a32003d6d74596c1cbb9a5c874f
LASHEB MOHAMED AMINE <ma.lasheb@esi-sba.dz>
2021-05-28 22:58:23 +0100

    SignLater + LogOut on settings

--- app/src/main/java/com/example/spacy/login.java
+++ app/src/main/java/com/example/spacy/login.java
@@ -7,16 +7,19 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 private CardView sign;
     private CardView sign2;
+    TextView later ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         sign=findViewById(R.id.sign);
         sign2=findViewById(R.id.signup);
+        later=findViewById(R.id.later);
         sign.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -31,5 +34,13 @@ private CardView sign;
                 startActivity(intent);
             }
         });
+        later.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, AFA.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
